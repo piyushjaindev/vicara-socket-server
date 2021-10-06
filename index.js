de36e4566a9a6198f2c9e283a3bb3b35d54ff7a1
piyush_applore <piyush@applore.in>
2021-10-06 21:12:24 +0530

    first commit

--- index.js
+++ index.js
@@ -4,20 +4,17 @@ const io = new Server({});
 
 io.on("connection", (socket) => {
 	console.log("connected NEW SOCKET : ", socket.id);
-	socket.on("click", () => {
-		console.log('click emit');
-		socket.emit("response", "Click OK");
+
+	socket.on("gyroscope", (data) => {
+		let { x, y, z } = JSON.parse(data);
+		socket.emit("response", `Gyroscope =  x: ${x} y: ${y} z: ${z}`);
 	});
-	socket.on("test", () => {
-		console.log('test emit');
-		socket.emit("response", "TEST OK");
+
+	socket.on("accelerometer", (data) => {
+		let { x, y, z } = JSON.parse(data);
+		socket.emit("response", `Accelerometer =  x: ${x} y: ${y} z: ${z}`);
 	});
-	socket.on("data", (data) => {
-		console.log('data emit', data);
-		let { name, value } = JSON.parse(data);
-		console.log(name, value);
-		socket.emit("response", `did u pass ? name : ${name}   value : ${value}`);
-	});
+
 	socket.on("disconnect", (reason) => {
 		console.log(reason);
 		console.log(socket.id, "  :  Disconnected");
